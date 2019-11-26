7947c6432045f7250f0cddd40cac7679437986d8
handanyue <handy1997@163.com>
2019-11-26 15:38:24 +0800

    add hdy info

--- test.c
+++ test.c
@@ -2,9 +2,11 @@
 
 int main(int argc, const char *argv[])
 {
+	printf("*************************");
 	printf("develop\n");
 	printf("gaga\n");
 	printf("hello world!\n");
 	printf("hello linux!\n");
+	printf("*************************");
 	return 0;
 }
